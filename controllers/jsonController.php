fb9ddaf1583cda7e773972bae40d20629a88be09
Jonathan Estay <j.estay1988@gmail.com>
2015-09-21 13:46:36 -0300

    Corrigiendo detalles de vista en jsonController

--- controllers/jsonController.php
+++ controllers/jsonController.php
@@ -14,7 +14,7 @@ class jsonController extends Controller {
     }
 
     public function index() {
-        $this->_view->rederJson('enviar_json');
+        header('Location: ' . BASE_URL . 'login');
     }
 
     public function getAcusePago() {
@@ -24,8 +24,6 @@ class jsonController extends Controller {
         //echo var_dump($headers); exit;
         $mensaje = array("reservation_confirmation_id"=>"","agency_id"=>"","time"=>"");
         
-        
-
         if ($this->getServer('PHP_AUTH_USER') && $this->getServer('PHP_AUTH_PW')) {
 
             $user = $this->getServer('PHP_AUTH_USER');
@@ -33,7 +31,7 @@ class jsonController extends Controller {
 
             $objUsuarios = $this->_json->consultarUser($user);
             if ($objUsuarios) {
-
+                
                 if ($objUsuarios[0]->getUser() == $user && $objUsuarios[0]->getPass() == $pass) {
 
                     if (isset($json->status) && isset($json->hash) && isset($json->amount) && isset($json->external_id)) {
@@ -44,7 +42,6 @@ class jsonController extends Controller {
                         if ($status!=="" && $hash!=="" && $monto!=="" && $num_file !==""){
 
                             $data = $this->_json->updatePagos($status, $hash, $monto, $num_file);
-
                             if ($data) {
                                 $mensaje = array("reservation_confirmation_id"=>$data->getNum(),"agency_id"=>$objUsuarios[0]->getIdAgentExter(),"time"=>date("d/m/Y H:i:s"));
                             }
