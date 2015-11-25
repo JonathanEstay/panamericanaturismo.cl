2747dae55525d487a92bc8b18549d9ca023734a0
Jonathan Estay <j.estay1988@gmail.com>
2015-11-25 13:11:47 -0300

    Agregando scripts de google analytics

--- views/layout/buscador/_NavBar.php
+++ views/layout/buscador/_NavBar.php
@@ -5,11 +5,24 @@
     <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>"  />
     <title><?php if(isset($this->titulo)){ echo $this->titulo; }?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    
+    <!-- Esta etiqueta le indica al navegador el ancho que debe usar para la pagina web -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="ORISTRAVEL">
-    <meta name="author" content="The Red Team">
+    <meta name="author" content="Tsyacom">
 
     <script src="<?php echo $_layoutParams['ruta_js']; ?>jquery-1.10.2.min.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-70684426-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+    
+    
     <link href="<?php echo $_layoutParams['ruta_css']; ?>styles.min.css" rel="stylesheet" >
     <link rel="shortcut icon" href="<?php echo $_layoutParams['ruta_img']; ?>favicon.ico">
     
